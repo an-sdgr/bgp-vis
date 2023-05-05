7ae9ad8144070fde41635fc926f5ffd69368a6ac
Austin Nason <austin.nason@schrodinger.com>
2023-05-04 21:07:33 -0700

    switch to yaml, put in folder

--- gobgp/entrypoint.sh
+++ gobgp/entrypoint.sh
@@ -10,7 +10,7 @@ export IP
 
 # render the template using environment vars
 # the $CONF variable determines which template is chosen, the rr or the client
-envsubst < "/gobgp/templates/$CONF.template" > /gobgp/gobgpd.conf
+envsubst < "/gobgp/templates/$CONF.yaml" > /gobgp/gobgpd.yaml
 
 # debug info, looks nice
 echo "starting $CONF id $IP AS $AS"
@@ -18,4 +18,4 @@ echo "starting $CONF id $IP AS $AS"
 /gobgp/gobgp global rib add 10.0.0.0/24 -a ipv4
 
 # run gobgp with our config file
-exec /gobgp/gobgpd -f /gobgp/gobgpd.conf
+exec /gobgp/gobgpd -t yaml -f /gobgp/gobgpd.yaml
