b083f521915fe80199becb9ba05a06b50e5b5648
Austin Nason <austin.nason@schrodinger.com>
2023-05-04 20:53:10 -0700

    clean up /gobgp

--- gobgp/entrypoint.sh
+++ gobgp/entrypoint.sh
@@ -10,10 +10,12 @@ export IP
 
 # render the template using environment vars
 # the $CONF variable determines which template is chosen, the rr or the client
-envsubst < "/gobgp/$CONF.template" > /gobgp/gobgpd.conf
+envsubst < "/gobgp/templates/$CONF.template" > /gobgp/gobgpd.conf
 
 # debug info, looks nice
 echo "starting $CONF id $IP AS $AS"
+
+/gobgp/gobgp global rib add 10.0.0.0/24 -a ipv4
 
 # run gobgp with our config file
 exec /gobgp/gobgpd -f /gobgp/gobgpd.conf
