7e8caaf59a79f245d50a5d085625161114ee1bf1
Austin Nason <austin.nason@schrodinger.com>
2023-05-04 17:30:09 -0700

    comments

--- gobgp/entrypoint.sh
+++ gobgp/entrypoint.sh
@@ -5,8 +5,10 @@ IP=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
 export IP
 
 # render the template using environment vars
+# the $CONF variable determines which template is chosen, the rr or the client
 envsubst < "/gobgp/$CONF.template" > /gobgp/gobgpd.conf
 
+# debug info, looks nice
 echo "starting $CONF id $IP AS $AS"
 
 # run gobgp with our config file
